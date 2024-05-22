3eeb3a14b933a37d80976d3e8a27c0f8a46679e4
uestc—503b <103475346+xawmx@users.noreply.github.com>
2024-05-22 19:14:57 +0800

    model.py

--- MMMNet/model.py
+++ MMMNet/model.py
@@ -5,7 +5,7 @@ import numpy as np
 import math
 
 
-# 定义对齐模型
+# Define alignment model
 class Alignment(nn.Module):
     def __init__(self):
         super(Alignment, self).__init__()
@@ -38,7 +38,7 @@ class Alignment(nn.Module):
         return text_out, pic_out
 
 
-# 定义跨模态融合模块_____创新在于关注点不仅有跨模态相关性，还考虑到单模态自身的权重
+# Definition of cross modal fusion module
 class CrossModule(nn.Module):
     def __init__(self):
         super(CrossModule, self).__init__()
@@ -66,7 +66,7 @@ class CrossModule(nn.Module):
         return fusion
 
 
-# 定义分类器模型
+# Define classifier model
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
@@ -113,6 +113,61 @@ class Classifier(nn.Module):
         p_uni = self.pic_uni(p)
         # x = torch.cat([p_uni, t_uni, fusion], 1)
         x = fusion
+        x = self.classifier_1(x)
+
+        # x = torch.cat([text, image], 1)
+        # x = torch.sigmoid(self.bn1(self.fc1(x)))
+        # x = torch.sigmoid(self.bn2(self.fc2(x)))
+        # x = torch.sigmoid(self.fc3(x))
+        return x
+
+# Define classifier model with mask
+class Classifier_M(nn.Module):
+    def __init__(self):
+        super(Classifier_M, self).__init__()
+        self.fusion_module = CrossModule()
+        # self.fc1 = nn.Linear(48, 32)
+        # self.bn1 = nn.BatchNorm1d(32)
+        # self.fc2 = nn.Linear(32, 16)
+        # self.bn2 = nn.BatchNorm1d(16)
+        # self.fc3 = nn.Linear(16, 1)
+        self.text_uni = nn.Sequential(
+            nn.Linear(768, 512),
+            nn.BatchNorm1d(512),
+            nn.ELU(),
+            nn.Linear(512, 128),
+            nn.BatchNorm1d(128),
+            nn.ELU(),
+            nn.Linear(128, 32),
+            nn.BatchNorm1d(32),
+            nn.ELU()
+        )
+        self.pic_uni = nn.Sequential(
+            nn.Linear(1000, 512),
+            nn.BatchNorm1d(512),
+            nn.ELU(),
+            nn.Linear(512, 128),
+            nn.BatchNorm1d(128),
+            nn.ELU(),
+            nn.Linear(128, 32),
+            nn.BatchNorm1d(32),
+            nn.ELU()
+        )
+        self.classifier_1 = nn.Sequential(
+            nn.Linear(64, 20),
+            nn.BatchNorm1d(20),
+            nn.ELU(),
+            nn.Linear(20, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, text, image, t, p, auto_mask):
+        fusion = self.fusion_module(text, image)
+        # x = torch.cat([text, image, fusion], 1)
+        t_uni = self.text_uni(t)
+        p_uni = self.pic_uni(p)
+        # x = torch.cat([p_uni, t_uni, fusion], 1)
+        x = fusion * auto_mask
         x = self.classifier_1(x)
 
         # x = torch.cat([text, image], 1)
